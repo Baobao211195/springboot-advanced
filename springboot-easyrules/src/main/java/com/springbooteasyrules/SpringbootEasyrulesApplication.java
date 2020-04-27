e6f65b2d3cb2b40f16cfcd621f725b3c40084488
phamvanoanh <pvoanh@cmc.com.vn>
2020-04-27 15:47:57 +0700

    Initial spring vs easy rules

---
+++ springboot-easyrules/src/main/java/com/springbooteasyrules/SpringbootEasyrulesApplication.java
@@ -0,0 +1,13 @@
+package com.springbooteasyrules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEasyrulesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEasyrulesApplication.class, args);
+    }
+
+}
---
+++ springboot-easyrules/src/test/java/com/springbooteasyrules/SpringbootEasyrulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbooteasyrules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEasyrulesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
