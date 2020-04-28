33fca3a88f67145f7a7058d0ae87a0699c687d12
phamvanoanh <pvoanh@cmc.com.vn>
2020-04-28 11:23:24 +0700

    add springboot multiple tenant

---
+++ springboot-multiple-tenancy/src/test/java/com/springmultipletenancy/SpringbootMultipleTenancyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmultipletenancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMultipleTenancyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
