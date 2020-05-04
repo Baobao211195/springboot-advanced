c52132912e6fa8334693ea0f7efceea54d7af618
phamvanoanh <pvoanh@cmc.com.vn>
2020-05-04 13:54:01 +0700

    initial spring multiple tenant

---
+++ springboot-multiple-tenancy/src/main/java/com/springmultipletenancy/SpringbootMultipleTenancyApplication.java
@@ -0,0 +1,13 @@
+package com.springmultipletenancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMultipleTenancyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMultipleTenancyApplication.class, args);
+    }
+
+}
