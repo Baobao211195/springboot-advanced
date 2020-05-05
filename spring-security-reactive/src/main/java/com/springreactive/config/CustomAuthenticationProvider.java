39b4e522e6b87af8919d442b984a09669bc8a4fc
phamvanoanh <pvoanh@cmc.com.vn>
2020-05-05 17:18:06 +0700

    add multiple security provider

---
+++ spring-security-reactive/src/main/java/com/springreactive/config/CustomAuthenticationProvider.java
@@ -0,0 +1,31 @@
+package com.springreactive.config;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        if ("user".equals(username) && "password".equals(password)) {
+            return new UsernamePasswordAuthenticationToken
+                (username, password, Collections.emptyList());
+        } else {
+            throw new BadCredentialsException("Authentication failed");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
---
+++ spring-security-reactive/src/main/java/com/springreactive/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.springreactive.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    CustomAuthenticationProvider customAuthenticationProvider;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // order 1
+        auth.authenticationProvider(customAuthenticationProvider);
+
+        // order 2
+        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+            .and()
+            .authorizeRequests()
+            .antMatchers("/**")
+            .authenticated();
+    }
+}
