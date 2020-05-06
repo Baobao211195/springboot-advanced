923a1263cfcd46e6c53aa7837f9a939294d71f0d
phamvanoanh <pvoanh@cmc.com.vn>
2020-05-06 11:40:39 +0700

    add spring security for application

--- spring-security-demo/src/main/java/com/springsecuritydemo/config/web/SecurityWebApplicationInitializer.java
+++ spring-security-demo/src/main/java/com/springsecuritydemo/config/web/SecurityWebApplicationInitializer.java
@@ -1,5 +1,7 @@
 package com.springsecuritydemo.config.web;
 
-public class SecurityWebApplicationInitializer {
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
 
 }
