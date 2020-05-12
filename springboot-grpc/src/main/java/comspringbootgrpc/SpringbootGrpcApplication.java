6db60fb84e857efdc9a0b9fc81efd3f695794304
phamvanoanh <pvoanh@cmc.com.vn>
2020-05-12 15:38:46 +0700

    add springboot grpc

---
+++ springboot-grpc/src/main/java/comspringbootgrpc/SpringbootGrpcApplication.java
@@ -0,0 +1,13 @@
+package comspringbootgrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGrpcApplication.class, args);
+    }
+
+}
---
+++ springboot-grpc/src/test/java/comspringbootgrpc/SpringbootGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package comspringbootgrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
